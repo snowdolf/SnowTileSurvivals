bef898d9ae8fe1ca042d53ed2aaab269dba5621b
snowdolf <snowdolf99@naver.com>
2024-05-14 01:19:59 +0900

    Add Character and move

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : SnowTileController
+{
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+}
---
+++ Assets/Scripts/SnowTileMovement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class SnowTileMovement : MonoBehaviour
+{
+    private SnowTileController controller;
+    private Rigidbody2D movementRigidbody;
+
+    private Vector2 movementDirection = Vector2.zero;
+
+    private void Awake()
+    {
+        controller = GetComponent<SnowTileController>();
+        movementRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(movementDirection);
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+        movementRigidbody.velocity = direction;
+    }
+}
