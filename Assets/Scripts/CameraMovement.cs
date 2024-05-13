1f5f00bcea9bb4d40653d5d9e9ebd692ca7dbfdf
snowdolf <snowdolf99@naver.com>
2024-05-14 03:32:58 +0900

    Add Camera Move and Deco Collision

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float offsetX = 0f;
+    public float offsetY = 0f;
+    public float offsetZ = -10f;
+
+    Vector3 cameraPosition;
+    float cameraSpeed = 10f;
+
+    public GameObject cameraTarget;
+
+    private void LateUpdate()
+    {
+        cameraPosition.x = cameraTarget.transform.position.x + offsetX;
+        cameraPosition.y = cameraTarget.transform.position.y + offsetY;
+        cameraPosition.z = cameraTarget.transform.position.z + offsetZ;
+
+        transform.position = Vector3.Lerp(transform.position, cameraPosition, cameraSpeed * Time.deltaTime);
+    }
+}
